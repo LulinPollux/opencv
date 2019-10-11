155b626a5fcd27466f55eb7c6644c1cdfea5a534
Lulin <kistssy+dev@gmail.com>
2019-10-11 12:28:12 +0900

    implement realtime rotate
    트랙바를 조절하여 실시간으로 이미지를 회전시키는 코드를 구현했다.

---
+++ src/realtime_rotate.py
@@ -0,0 +1,27 @@
+import cv2
+
+
+def nothing(x):
+    pass
+
+
+src = cv2.imread("../res/1.jpg", cv2.IMREAD_COLOR)
+height, width, channel = src.shape
+
+cv2.namedWindow('image')
+cv2.createTrackbar('degree', 'image', 0, 360, nothing)
+cv2.createTrackbar('reverse', 'image', 0, 1, nothing)
+
+while True:
+    degree = cv2.getTrackbarPos('degree', 'image')
+    reverse = cv2.getTrackbarPos('reverse', 'image')
+    if not reverse:
+        matrix = cv2.getRotationMatrix2D((width/2, height/2), degree, 1)
+    else:
+        matrix = cv2.getRotationMatrix2D((width / 2, height / 2), -degree, 1)
+    dst = cv2.warpAffine(src, matrix, (width, height))
+    cv2.imshow("image", dst)
+    if cv2.waitKey(20) > 0:
+        break
+
+cv2.destroyAllWindows()
