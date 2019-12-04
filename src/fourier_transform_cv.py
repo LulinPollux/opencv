6e3cf65845bc0fbdcf8ae79bd84470d53596ed72
LulinPollux <kistssy+dev@gmail.com>
2019-12-04 16:54:29 +0900

    refactor: edit line

--- src/fourier_transform_cv.py
+++ src/fourier_transform_cv.py
@@ -4,8 +4,8 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('../res/lenna.bmp')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-dft = cv2.dft(np.float32(img), flags=cv2.DFT_COMPLEX_OUTPUT)
 
+dft = cv2.dft(np.float32(img), flags=cv2.DFT_COMPLEX_OUTPUT)
 dft_shift = np.fft.fftshift(dft)
 magnitude_spectrum = 20 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
 
