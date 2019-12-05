e8ec9b66e968405f124504d60801fee17dd76d70
LulinPollux <kistssy+dev@gmail.com>
2019-12-05 10:59:05 +0900

    refactor: remodel codes
    1. DFT, IDFT, LPF with IDFT를 출력한다.
    2. 주석 달기

--- src/fourier_transform_cv.py
+++ src/fourier_transform_cv.py
@@ -1,36 +1,45 @@
-import numpy as np
 import cv2
+import numpy as np
 from matplotlib import pyplot as plt
 
+# 원본 이미지를 입력한다.
 img = cv2.imread('../res/lenna.bmp')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-dft = cv2.dft(np.float32(img), flags=cv2.DFT_COMPLEX_OUTPUT)
-dft_shift = np.fft.fftshift(dft)
-magnitude_spectrum = 20 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
+""" 이산 푸리에 변환(DFT) """
+dft = cv2.dft(np.float32(img), flags=cv2.DFT_COMPLEX_OUTPUT)  # 이산 푸리에 변환을 적용한다.
+dft_shift = np.fft.fftshift(dft)  # 셔플링을 한다.
+# 스펙트럼 영상을 구한다. D(u, v) = c x log( |F(u, v)| )
+spectrum_img = 20 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
 
+""" 역 이산 푸리에 변환(IDFT) """
+dft_ishift = np.fft.ifftshift(dft_shift)  # 셔플링 되었던 것을 역셔플링한다.
+idft = cv2.idft(dft_ishift)  # 역 이산 푸리에 변환을 한다.
+idft = cv2.magnitude(idft[:, :, 0], idft[:, :, 1])  # 절대값 적용
+
+""" 저주파 통과 필터링(LPF) """
 rows, cols = img.shape
-crow, ccol = rows // 2, cols // 2
-
-# 아래는 d 사이지의 사각형을 생성한 후, 사각형 바깥쪽을 제거하는 형태임.
-# 즉, 고주파영역을 제거하게 됨.
-# d값이 작을수록 사각형이 작고, 바깥영역 즉, 고주파영역이  많이 제거되기 때문에 이미지가 뭉게지고
-# d값이 클수록 사각형이 크고, 바깥영역 즉, 고주파 영역이 적게 제거되기 때문에 원래 이미지와 가까워짐.
-d = 30
+center_row, center_col = rows // 2, cols // 2  # 이미지의 중심 좌표
+size = 20
 mask = np.zeros((rows, cols, 2), np.uint8)
-mask[crow - d:crow + d, ccol - d:ccol + d] = 1
-# apply mask and inverse DFT
-fshift = dft_shift * mask
-f_ishift = np.fft.ifftshift(fshift)
-img_back = cv2.idft(f_ishift)
-img_back = cv2.magnitude(img_back[:, :, 0], img_back[:, :, 1])
+mask[center_row - size:center_row + size, center_col - size:center_col + size] = 1
+lpf = dft_shift * mask  # 마스크를 적용한다.
 
-plt.subplot(221), plt.imshow(img, cmap='gray')
+""" LPF가 적용된 역 이산 푸리에 변환(LPF with IDFT) """
+dft_ishift2 = np.fft.ifftshift(lpf)  # 셔플링 되었던 것을 역셔플링한다.
+idft2 = cv2.idft(dft_ishift2)  # 역 이산 푸리에 변환을 한다.
+idft2 = cv2.magnitude(idft2[:, :, 0], idft2[:, :, 1])  # 절대값 적용
+
+# 결과물을 출력한다.
+plt.subplot(221), plt.imshow(img, cmap='gray')  # 원본 영상
 plt.title('Input Image'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(222), plt.imshow(magnitude_spectrum, cmap='gray')
-plt.title('Spectrum'), plt.xticks([]), plt.yticks([])
+plt.subplot(222), plt.imshow(spectrum_img, cmap='gray')  # DFT 영상
+plt.title('DFT'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(223), plt.imshow(img_back, cmap='gray')
-plt.title('FT'), plt.xticks([]), plt.yticks([])
+plt.subplot(223), plt.imshow(idft, cmap='gray')  # IDFT 영상
+plt.title('IDFT'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(224), plt.imshow(idft2, cmap='gray')  # LPF가 적용된 IDFT 영상
+plt.title('IDFT(LPF apply)'), plt.xticks([]), plt.yticks([])
 plt.show()
