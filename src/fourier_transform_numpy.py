0a58ea41024dff542265083992d5ef88ee61acdc
LulinPollux <kistssy+dev@gmail.com>
2019-12-05 10:14:23 +0900

    refactor: edit comments, variable names

--- src/fourier_transform_numpy.py
+++ src/fourier_transform_numpy.py
@@ -2,44 +2,44 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+# 원본 이미지를 입력한다.
 img = cv2.imread('../res/lenna.bmp')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-"""
-# Fourier Transform을 적용.
- 적용을 하면 0,0, 즉 화면 좌측상단점이 중심이고, 거기에 저주파가 모여 있음.
- 분석을 용이하게 하기 위해 0,0을 이미지의 중심으로 이동 시키고 Log Scaling을 하여 분석이 용이한 결과값으로 변환
-"""
-f = np.fft.fft2(img)  # 이미지에 푸리에 변환 적용
-fshift = np.fft.fftshift(f)  # 분석을 용이하게 하기 위해 중앙에 저주파가 모이게 한다. (셔플링)
-magnitude_spectrum = 20 * np.log(np.abs(fshift))  # spectrum 구하는 수학식.
+""" 순방향 고속 푸리에 변환(FFT)
+    적용을 하면 화면 좌측상단(0, 0)이 중심이고, 그곳에 저주파가 모여 있다.
+    분석을 쉽게 하기 위해 셔플링을 하고 Log Scaling을 하여 매우 넓은 범위의 값을 축소시킨다. """
+fft = np.fft.fft2(img)  # 고속 푸리에 변환을 적용한다.
+fft_shift = np.fft.fftshift(fft)  # 분석을 쉽게 하기 위해 셔플링을 한다.
+spectrum_img = 20 * np.log(np.abs(fft_shift))  # 스펙트럼 영상을 구한다. D(u, v) = c x log( |F(u, v)| )
 
-rows, cols = img.shape
-crow, ccol = rows // 2, cols // 2  # 이미지의 중심 좌표
+""" 고주파 통과 필터링(HPF)
+    중앙에서 10X10 크기의 사각형의 값을 1로 설정하여, 중앙의 저주파를 모두 제거(검게 만듬)
+    저주파를 제거했으므로 배경이 사라지고 에지만 남게 된다. """
+row, col = img.shape
+center_row, center_col = row // 2, col // 2  # 이미지의 중심 좌표
+size = 10  # 제거할 저주파 영역의 크기 (n x n)
+fft_shift[center_row - size:center_row + size, center_col - size:center_col + size] = 1
 
-# 중앙에서 10X10 사이즈의 사각형의 값을 1로 설정함. 중앙의 저주파를 모두 제거
-# 저주파를 제거하였기 때문에 배경이 사라지고 경계선만 남게 됨.
-d = 10
-fshift[crow - d:crow + d, ccol - d:ccol + d] = 1
+""" 역방향 고속 푸리에 변환(IFFT)
+    푸리에 변환결과를 다시 이미지로 변환한다. """
+fft_ishift = np.fft.ifftshift(fft_shift)  # 셔플링 되었던 것을 역셔플링한다.
+ifft = np.fft.ifft2(fft_ishift)  # 역방향 고속 푸리에 변환을 한다.
+ifft = np.abs(ifft)  # 절대값 적용
 
-# 푸리에 변환결과를 다시 이미지로 변환
-f_ishift = np.fft.ifftshift(fshift)
-img_back = np.fft.ifft2(f_ishift)
-img_back = np.abs(img_back)
+# 임계 값으로 이진 영상을 만든다.
+ret, binary_img = cv2.threshold(np.uint8(ifft), 27, 255, cv2.THRESH_BINARY_INV)
 
-# threshold를 적용하기 위해 float type을 int type으로 변환
-img_new = np.uint8(img_back)
-ret, thresh = cv2.threshold(img_new, 30, 255, cv2.THRESH_BINARY_INV)
-
-plt.subplot(221), plt.imshow(img, cmap='gray')
+# 결과물을 출력한다.
+plt.subplot(221), plt.imshow(img, cmap='gray')  # 원본 영상
 plt.title('Input Image'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(222), plt.imshow(magnitude_spectrum, cmap='gray')
-plt.title('Spectrum'), plt.xticks([]), plt.yticks([])
+plt.subplot(222), plt.imshow(spectrum_img, cmap='gray')  # FFT 영상
+plt.title('FFT'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(223), plt.imshow(img_back, cmap='gray')
-plt.title('FT'), plt.xticks([]), plt.yticks([])
+plt.subplot(223), plt.imshow(ifft, cmap='gray')  # HPF가 적용된 IFFT 영상
+plt.title('IFFT(HPF apply)'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(224), plt.imshow(thresh, cmap='gray')
-plt.title('Threshold With FT'), plt.xticks([]), plt.yticks([])
+plt.subplot(224), plt.imshow(binary_img, cmap='gray')  # 이진화된 IFFT 영상
+plt.title('IFFT(HPF binary)'), plt.xticks([]), plt.yticks([])
 plt.show()
