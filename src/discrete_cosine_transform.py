4c1db457be13a5b3c6a9067877ed43f03ac97f1c
LulinPollux <kistssy+dev@gmail.com>
2019-12-05 12:35:44 +0900

    dct: add HPF output

--- src/discrete_cosine_transform.py
+++ src/discrete_cosine_transform.py
@@ -13,6 +13,16 @@ spectrum_img = 20 * np.log(cv2.magnitude(dct[:, :], dct[:, :]))  # 스펙트럼 
 """ 역 이산 코사인 변환(IDCT) """
 idct = cv2.idct(dct)
 
+""" 고주파 통과 필터링(HPF) """
+rows, cols = img.shape
+size = 23
+mask = np.ones((rows, cols), np.uint8)
+mask[0:size, 0:size] = 0
+hpf = dct * mask  # 마스크를 적용한다. (요소별 곱셈: n x 1 = n, n x 0 = 0)
+
+""" 역 이산 코사인 변환(IDCT) """
+idct2 = cv2.idct(hpf)
+
 # 결과물을 출력한다.
 plt.subplot(221), plt.imshow(img, cmap='gray')  # 원본 영상
 plt.title('Input Image'), plt.xticks([]), plt.yticks([])
@@ -22,4 +32,7 @@ plt.title('DCT'), plt.xticks([]), plt.yticks([])
 
 plt.subplot(223), plt.imshow(idct, cmap='gray')  # IDCT 영상
 plt.title('IDCT'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(224), plt.imshow(idct2, cmap='gray')  # HPF가 적용된 IFFT 영상
+plt.title('IDCT(HPF apply)'), plt.xticks([]), plt.yticks([])
 plt.show()
