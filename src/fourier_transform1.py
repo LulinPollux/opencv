ee0e9f981814f21e36d3f99b66948f945c625ab9
LulinPollux <kistssy+dev@gmail.com>
2019-12-05 19:00:33 +0900

    ft: add LPF spectrum output
    1. LPF를 수행한 주파수 영역을 스펙트럼으로 출력한다.
    2. 스펙트럼 공식에서 0이 들어가면 오류가 나서 1을 더해준다.

--- src/fourier_transform1.py
+++ src/fourier_transform1.py
@@ -12,7 +12,7 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 dft = cv2.dft(np.float32(img), flags=cv2.DFT_COMPLEX_OUTPUT)  # 이산 푸리에 변환을 적용한다.
 dft_shift = np.fft.fftshift(dft)  # 셔플링을 한다.
 # 스펙트럼 영상을 구한다. D(u, v) = c x log( |F(u, v)| )
-spectrum_img = 20 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
+spectrum = 20 * np.log(1 + cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
 
 """ 역 이산 푸리에 변환(IDFT) """
 dft_ishift = np.fft.ifftshift(dft_shift)  # 셔플링 되었던 것을 역셔플링한다.
@@ -27,22 +27,26 @@ size = 30  # 통과시킬 저주파 영역의 크기 (n x n)
 mask = np.zeros((rows, cols, 2), np.uint8)
 mask[center_row - size:center_row + size, center_col - size:center_col + size] = 1
 lpf = dft_shift * mask  # 마스크를 적용한다. (요소별 곱셈: n x 1 = n, n x 0 = 0)
+lpf_spectrum = 20 * np.log(1 + cv2.magnitude(lpf[:, :, 0], lpf[:, :, 1]))
 
 """ LPF가 적용된 역 이산 푸리에 변환(LPF with IDFT) """
 dft_ishift2 = np.fft.ifftshift(lpf)  # 셔플링 되었던 것을 역셔플링한다.
-idft2 = cv2.idft(dft_ishift2)  # 역 이산 푸리에 변환을 한다.
-idft2 = cv2.magnitude(idft2[:, :, 0], idft2[:, :, 1])  # 절대값 적용
+lpf_idft = cv2.idft(dft_ishift2)  # 역 이산 푸리에 변환을 한다.
+lpf_idft = cv2.magnitude(lpf_idft[:, :, 0], lpf_idft[:, :, 1])  # 절대값 적용
 
 # 결과물을 출력한다.
-plt.subplot(221), plt.imshow(img, cmap='gray')  # 원본 영상
+plt.subplot(231), plt.imshow(img, cmap='gray')  # 원본 영상
 plt.title('Input Image'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(222), plt.imshow(spectrum_img, cmap='gray')  # DFT 영상
+plt.subplot(232), plt.imshow(spectrum, cmap='gray')  # DFT 영상
 plt.title('DFT'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(223), plt.imshow(idft, cmap='gray')  # IDFT 영상
+plt.subplot(233), plt.imshow(idft, cmap='gray')  # IDFT 영상
 plt.title('IDFT'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(224), plt.imshow(idft2, cmap='gray')  # LPF가 적용된 IDFT 영상
+plt.subplot(234), plt.imshow(lpf_idft, cmap='gray')  # LPF가 적용된 IDFT 영상
 plt.title('IDFT(LPF apply)'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(235), plt.imshow(lpf_spectrum, cmap='gray')  # LPF가 적용된 IDFT 영상
+plt.title('DFT(LPF apply)'), plt.xticks([]), plt.yticks([])
 plt.show()
